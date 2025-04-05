c36c5b87730df96b0b37b457a1b0827d1c1b93ec
Mustafa İNKAYA <mustafa.inkaya@hotmail.com>
2025-04-05 17:22:50 +0300

    IdentityServer

---
+++ BaseProject/Controllers/AccountController.cs
@@ -0,0 +1,39 @@
+﻿using BaseProject.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaseProject.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> userManager;
+        private readonly SignInManager<AppUser> signInManager;
+
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(string email, string password)
+        {
+            var hasUser = await userManager.FindByEmailAsync(email);
+            if (hasUser == null)
+                return View();
+
+            var signInResult = await signInManager.PasswordSignInAsync(hasUser, password, true, false);
+            if (!signInResult.Succeeded)
+            {
+                return View();
+            }
+
+            return RedirectToAction(nameof(HomeController.Index), "Home");
+        }
+    }
+}
--- BaseProject/Program.cs
+++ BaseProject/Program.cs
@@ -1,9 +1,26 @@
+using BaseProject.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddDbContext<AppIdentityDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer"));
+});
+
+builder.Services.AddIdentity<AppUser, IdentityRole>(options =>
+{
+    options.User.RequireUniqueEmail = true;
+}).AddEntityFrameworkStores<AppIdentityDbContext>();
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -17,11 +34,31 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
+
+using (var scope = app.Services.CreateScope())
+{
+    var identityDbContext = scope.ServiceProvider.GetRequiredService<AppIdentityDbContext>();
+
+    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<AppUser>>();
+
+    identityDbContext.Database.Migrate(); //Uygulama ayaða kalktýðýnda veritabanýna migration uygulanmadýysa uygulanmasý için. 
+
+    if (!userManager.Users.Any())
+    {
+        userManager.CreateAsync(new AppUser() { UserName = "user1", Email = "user1@hotmail.com" }, "Password12*").Wait();
+        userManager.CreateAsync(new AppUser() { UserName = "user2", Email = "user2@hotmail.com" }, "Password12*").Wait();
+        userManager.CreateAsync(new AppUser() { UserName = "user3", Email = "user3@hotmail.com" }, "Password12*").Wait();
+        userManager.CreateAsync(new AppUser() { UserName = "user4", Email = "user4@hotmail.com" }, "Password12*").Wait();
+        userManager.CreateAsync(new AppUser() { UserName = "user5", Email = "user5@hotmail.com" }, "Password12*").Wait();
+    }
+}
 
 app.Run();
